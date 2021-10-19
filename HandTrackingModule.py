245fab7395390e80a15cd26e12812c672c46167a
ameeshagireesh <ameesha.gireeshkm@gmail.com>
2021-10-19 12:32:43 +0400

    fix: fixed error #1

--- HandTrackingModule.py
+++ HandTrackingModule.py
@@ -2,70 +2,57 @@ import cv2
 import mediapipe as mp
 import time
 
-cap = cv2.VideoCapture(0)
-
 class handDetector():
     def __init__(self, mode=False, maxHands=2, detectionCon=0.5, trackCon=0.5):
         self.mode = mode
         self.maxHands = maxHands
         self.detectionCon = detectionCon
         self.trackCon = trackCon
-
+        
         self.mpHands = mp.solutions.hands
         self.hands = self.mpHands.Hands(self.mode, self.maxHands, self.detectionCon, self.trackCon)
         self.mpDraw = mp.solutions.drawing_utils
 
-        def findHands(self, img, draw = True):
-            #the hand is given 21 landmarks and the landmarks are the dots being displayed on the hand
-            mpHands = mp.solutions.hands
-            hands = mpHands.Hands()
-            mpDraw = mp.solutions.drawing_utils
 
-            imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-            self.results = self.hands.process(imgRGB)
-            #print(results.multi_hand_landmarks)
+    def findHands(self, img, draw=True):
+        imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        self.results = self.hands.process(imgRGB)
 
-            if self.results.multi_hand_landmarks:
-                for handLms in self.results.multi_hand_landmarks:
-                    if draw:
-                        self.mpDraw.draw_landmarks(img, handLms, self.mpHands.HAND_CONNECTIONS)
+        if self.results.multi_hand_landmarks:
+            for handLms in self.results.multi_hand_landmarks:
+                if draw:
+                    self.mpDraw.draw_landmarks(img, handLms, self.mpHands.HAND_CONNECTIONS)
+        return img
 
-            return img
-        
+    def findPosition(self, img, handNo=0, draw=True):
+        lmList = []
 
+        if self.results.multi_hand_landmarks:
+            myHand = self.results.multi_hand_landmarks[handNo]
 
-        def findPosition(self, img, handNo=0, draw=True):
-            
-            lmList = []
-            if self.results.multi_hand_landmarks:
-                myHand = self.results.multi_hand_landmarks[handNo]
-                for id, lm in enumerate(myHand.landmark):
-                    #print(id, lm)
-                    h, w, c = img.shape #height and width
-                    cx, cy = int(lm.x*w), int(lm.y*h) #centre coordinates
-                    #print(id, cx, cy) #printing location info about the hand landmarks
+            for id, lm in enumerate(myHand.landmark):
+                    h, w, c = img.shape
+                    cx = int(lm.x * w)
+                    cy = int(lm.y * h)
                     lmList.append([id, cx, cy])
-                    if draw:
-                        cv2.circle(img, (cx, cy), 15, (255, 0, 255), cv2.FILLED)
-              
-            return lmList
-
-
+                    # if draw:
+                    #     cv2.circle = (img, (cx, cy), 15, (255,0,255), cv2.FILLED)
+        return lmList
 
 def main():
     pTime = 0 #previous time
     cTime = 0 #current time
+    
     cap = cv2.VideoCapture(0)
     detector = handDetector()
+
     while True:
         success, img = cap.read()
-
-        img = detector.findHands(img, draw=True)
-        lmList = detector.findPosition(img, draw=False)
+        img = detector.findHands(img)
+        lmList = detector.findPosition(img)
 
         if len(lmList)!=0:
-            print(lmList[4]) #4 is for the tip of the thumb, for example
-
+            print(lmList[4])
 
         cTime = time.time()
         fps = 1/(cTime-pTime)
@@ -77,4 +64,4 @@ def main():
         cv2.waitKey(1)
 
 if __name__ == "__main__":
-    main()  
+    main()
