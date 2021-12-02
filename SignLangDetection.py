fc183d799c8dbd86532093375797e48f93cb4f45
Ameesha Gireesh <ameesha.gireeshkm@gmail.com>
2021-12-02 09:09:01 +0400

    feat: added like and dislike recognition

--- SignLangDetection.py
+++ SignLangDetection.py
@@ -10,6 +10,7 @@ mp_draw = mp.solutions.drawing_utils
 cap = cv2.VideoCapture(0)
 
 finger_tips = [8, 12, 16, 20]
+thumb_tip = 4
 
 while True:
     ret, img = cap.read()
@@ -23,11 +24,28 @@ while True:
 
             for id, lm in enumerate(hand_landmark.landmark):
                 lm_list.append(lm)
-
+            finger_fold_status = []
+            
             for tip in finger_tips:
                 x, y = int(lm_list[tip].x*w), int(lm_list[tip].y*h)
                 # print(id, ":", x, y)
                 cv2.circle(img, (x,y), 15, (255, 0, 0), cv2.FILLED)
+
+                if lm_list[tip].x < lm_list[tip-3].x:
+                    cv2.circle(img, (x,y), 15, (0, 255, 0), cv2.FILLED)
+                    finger_fold_status.append(True)
+                else:
+                    finger_fold_status.append(False)
+            
+            print(finger_fold_status)
+
+            if all(finger_fold_status):
+                if lm_list[thumb_tip].y < lm_list[thumb_tip - 1].y < lm_list[thumb_tip-2].y:
+                    print("LIKE")
+                    cv2.putText(img, "LIKE", (20, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 3)
+                if lm_list[thumb_tip].y > lm_list[thumb_tip - 1].y > lm_list[thumb_tip-2].y:
+                    print("DISLIKE")
+                    cv2.putText(img, "DISLIKE", (20, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 3)
 
             mp_draw.draw_landmarks(img, hand_landmark, mp_hands.HAND_CONNECTIONS)
 
